e94ca240b306d036ce68fe1a71f332753764a056
ancre <ancre@DESKTOP-TIN60FP>
2025-03-07 12:47:43 +0900

    환경 세팅

--- diary/src/main/java/com/site/diary/DiaryApplication.java
+++ diary/src/main/java/com/site/diary/DiaryApplication.java
@@ -1,13 +1,18 @@
 package com.site.diary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiaryApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DiaryApplication.class); 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiaryApplication.class, args);
+		logger.info("app on");
 	}
 
 }
