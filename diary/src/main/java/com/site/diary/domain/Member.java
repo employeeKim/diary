3950bc58b771a8245d472d0acfe92d9b748c9c3a
ancre <ancre@DESKTOP-TIN60FP>
2025-03-08 23:17:05 +0900

    DB 연동

---
+++ diary/src/main/java/com/site/diary/domain/Member.java
@@ -0,0 +1,24 @@
+package com.site.diary.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+	@Id
+	private String Id;
+	private String pw;
+	private String rePw;
+	private String name;
+	private String phone;
+	private String birth;
+	private String email;
+}
